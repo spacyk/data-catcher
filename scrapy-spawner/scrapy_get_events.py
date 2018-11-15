613500b56e02670dc26f97dfc77fdd7f96495b02
Jan Spacek <jan.spacek12@gmail.com>
2018-11-15 02:25:16 +0100

    get evenets

--- scrapy-spawner/scrapy_get_events.py
+++ scrapy-spawner/scrapy_get_events.py
@@ -3,27 +3,25 @@ import scrapy
 
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
-    start_urls = ['http://brickset.com/sets/year-2016']
+    start_urls = ['https://www.slevomat.cz/praha/zabava-a-volny-cas/adrenalinove-zazitky']
+    page_number = 1
 
 
     def parse(self, response):
-        SET_SELECTOR = '.set'
-        for brickset in response.css(SET_SELECTOR):
-            NAME_SELECTOR = 'h1 a ::text'
-            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
-            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
-            IMAGE_SELECTOR = 'img ::attr(src)'
+        self.page_number += 1
+        SET_SELECTOR = '.product'
+        set = response.css(SET_SELECTOR)
+        if not set:
+            return
+        for brickset in set:
+            NAME_SELECTOR = 'h3 ::text'
             yield {
                 'name': brickset.css(NAME_SELECTOR).extract_first(),
-                'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
-                'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
-                'image': brickset.css(IMAGE_SELECTOR).extract_first(),
             }
 
-            NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
-            next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-            if next_page:
-                yield scrapy.Request(
-                    response.urljoin(next_page),
-                    callback=self.parse
-                )
+            next_page = f'{self.start_urls[0]}?page={self.page_number}'
+
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
