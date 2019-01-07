6f7a28e074e1bec6ffe0f17ea78c8db31ac5706d
Jan Spacek <jan.spacek12@gmail.com>
2019-01-07 01:38:07 +0100

    make as package

---
+++ scrapyspawner/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scrapyspawner',
+      version='0.1',
+      description='Scrape any page providing only few settings',
+      url='https://github.com/spacyk/data-catcher/scrapyspawner',
+      author='Jan Spacek',
+      author_email='jan.spacek12@gmail.com',
+      license='MIT',
+      packages=['scrapyspawner'],
+      zip_safe=False)
